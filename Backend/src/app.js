58d4bcbadc03a6bfe72ce50cd4adc8391062a139
KPriYam-123 <231210062@nitdelhi.ac.in>
2025-09-07 14:25:31 +0530

    feat: Implement user registration API and remove unused frontend components

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,9 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-
+//Routes import
+import userRouter from "./routes/user.routes.js"
+app.use('/api/v1/users', userRouter)
 
 
 export {app}
